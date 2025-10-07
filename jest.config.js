97165147c6fd975b724d603becf35f038f645dd0
fluxo <cml@apache.org>
2025-10-07 11:47:43 -0700

    add GH action workflow for jest coverage
    include .gitignore to avoid committing local coverage reports and build
    artifacts from local testing
    
    add additional typecheck

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/opcodes', '<rootDir>'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  coverageReporters: ['text', 'lcov', 'json-summary'],
   collectCoverageFrom: [
     'opcodes/**/*.ts',
     '!opcodes/**/*.d.ts',
