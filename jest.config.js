9402650a663fedf42c02713e0fd0d885c24f6347
fluxo <cml@apache.org>
2025-10-07 12:06:33 -0700

    refactor jest test coverage
    add baseline coverage thresholds (matching where we are right now with
    the codebase)
    
    update the coverage collection to not introduce "false" lows from
    entrypoints like tszm.ts and build artifacts like node_modules

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,33 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/opcodes', '<rootDir>'],
+
+  roots: ['<rootDir>'],
+
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+
   coverageReporters: ['text', 'lcov', 'json-summary'],
+
   collectCoverageFrom: [
     'opcodes/**/*.ts',
-    '!opcodes/**/*.d.ts',
-    '!opcodes/**/*.test.ts',
-    '*.ts',
-    '!*.test.ts',
-    '!*.d.ts',
+    '!**/*.d.ts',
+    '!**/*.(spec|test).ts',
   ],
+
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/tszm.ts',
+  ],
+
+  // Start with global thresholds just below your “all files” numbers
+  coverageThreshold: {
+    global: {
+      statements: 72,
+      branches:   69,
+      functions:  50,
+      lines:      72,
+    },
+  },
 };
+
